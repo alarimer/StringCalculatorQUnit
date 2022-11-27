121bb563bfc84a0ce189181769c47c7b496e3cdb
Alan Larimer <alan.larimer@outlook.com>
2022-11-27 18:31:44 -0500

    Return sum

--- stringcalculator.js
+++ stringcalculator.js
@@ -2,6 +2,9 @@ function add(string) {
     if (!isNaN(string)) {
         return string;
     }
-    
+
+    let numbers = string.split(',');
+    return parseInt(numbers[0]) + parseInt(numbers[1]);
+
     return 0;
 }
